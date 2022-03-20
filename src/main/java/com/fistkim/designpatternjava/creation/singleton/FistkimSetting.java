f8b73c6c775200d6d374d1b4e2b1b6ddcf9dd015
fistkim101 <fistkim101@gmail.com>
2022-03-20 11:46:42 +0900

    싱글톤 패턴 Lazy holder

--- src/main/java/com/fistkim/designpatternjava/creation/singleton/FistkimSetting.java
+++ src/main/java/com/fistkim/designpatternjava/creation/singleton/FistkimSetting.java
@@ -1,19 +1,16 @@
 package com.fistkim.designpatternjava.creation.singleton;
 
-// 멀티스레드 상황에서 안전하지 않은 경우의 코드
 public class FistkimSetting {
-
-    private static FistkimSetting setting;
 
     private FistkimSetting() {
     }
 
-    public static FistkimSetting getInstance() {
-        if (setting == null) {
-            return new FistkimSetting();
-        }
+    private static class SettingHolder {
+        private static final FistkimSetting SETTING = new FistkimSetting();
+    }
 
-        return setting;
+    public static FistkimSetting getInstance() {
+        return SettingHolder.SETTING;
     }
 
 }
